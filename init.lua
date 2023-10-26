ddf682d51bb7226b5aa88801e7e36028157e4122
soleil <1938615010@qq.com>
2023-10-26 23:08:01 +0200

    initially setup lazy-nvim

--- init.lua
+++ init.lua
@@ -1,7 +1,13 @@
---[[ init.lua ]]
-
-require('opts')
-require('keys')
-require('cmds')
-require('colors')
-require('plugins')
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup("plugins")
--- lua/colors.lua
+++
@@ -1,8 +0,0 @@
---[[ colors.lua ]]
-
-local cmd = vim.api.nvim_command
-
-cmd('colorscheme torte')
-
--- remove the grey color of the window's seperator
-cmd('hi VertSplit guibg=bg guifg=fg')
