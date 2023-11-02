8f23de7b4f816728ec608c4c7d1ef977e2c9f706
soleil <1938615010@qq.com>
2023-11-02 17:43:54 +0100

    1. added colorscheme list completion to 'CC' command; 2. setup Prettierd as formatter for .tsx and .jsx files

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -2,6 +2,10 @@ vim.api.nvim_create_user_command('CC',
   function(opts)
     vim.cmd.colorscheme(opts.fargs[1])
     vim.cmd("hi VertSplit guibg=bg guifg=fg")
+    vim.notify("colorscheme has been set to: " .. opts.fargs[1])
   end,
-  { nargs = 1, desc = "Change Colorscheme" }
+  { nargs = 1,
+    complete = "color",
+    desc = "Change Colorscheme",
+  }
 )
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 local map = vim.keymap.set
 
 map("i", "jk", "<ESC>", { desc = "Escape to normal mode", noremap = true, silent = true })
+
+-- Prettier formating buffer specifically for tsx and jsx files
+map("n", "<leader>pf", ':%!prettierd "%:p"<CR>', { desc = "Prettierd Formatter", noremap = true, silent = true })
