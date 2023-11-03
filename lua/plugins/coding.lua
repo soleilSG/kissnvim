3c8d824d6b90e63379e06140fba1192e4dc9ef6f
soleil <1938615010@qq.com>
2023-11-03 23:09:33 +0100

    optimized nvim-cmp key-mappings

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -277,23 +277,23 @@ return {
 						require("luasnip").lsp_expand(args.body)
 					end,
 				},
-				mapping = cmp.mapping.preset.insert({
+				mapping = {
 					["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
 					["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
-					["<S-CR>"] = cmp.mapping.confirm({
-						behavior = cmp.ConfirmBehavior.Replace,
-						select = true,
-					}),
-					["<C-CR>"] = function(fallback)
-						cmp.abort()
-						fallback()
-					end,
-				}),
+					["<C-j>"] = cmp.mapping(function()
+						require("luasnip").jump(-1)
+					end, { "i", "s" }),
+					["<C-l>"] = cmp.mapping(function()
+						require("luasnip").jump(1)
+					end, { "i", "s" }),
+					["<C-k>"] = cmp.mapping(function()
+						require("luasnip").expand()
+					end, { "i", "s" }),
+				},
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" },
