f25f1a3f075eccf6914f5efe4f4cca58129ffd1a
soleil <applosoleil@gmail.com>
2026-04-28 14:11:31 +0000

    Added taplo lsp and formatter for toml

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -5,4 +5,5 @@ vim.lsp.enable({
 	-- "vtsls",
 	-- "basedpyright",
 	-- "ruff",
+	-- "taplo",
 })
--- lua/plugins/ide.lua
+++ lua/plugins/ide.lua
@@ -1,10 +1,10 @@
 return {
-  -- Mason
-  {
-    "mason-org/mason.nvim",
-    opts = {},
-    lazy = false,
-  },
+	-- Mason
+	{
+		"mason-org/mason.nvim",
+		opts = {},
+		lazy = false,
+	},
 
 	-- Blink.cmp completion
 	{
@@ -82,8 +82,9 @@ return {
 				-- Google style, generate .clang-format at project root
 				-- clang-format --style=Google --dump-config > .clang-format
 				json = { "jq" },
-        javascript = {"prettierd"},
-        typescript = {"prettierd"},
+				javascript = { "prettierd" },
+				typescript = { "prettierd" },
+				toml = { "taplo" },
 			},
 			default_format_opts = {
 				lsp_format = "fallback",
