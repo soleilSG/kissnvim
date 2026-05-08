c2bc0f510d179cc057cbb646393ba6e49b583d82
soleil <applosoleil@gmail.com>
2026-05-08 20:57:23 +0700

    improved zoom current buffer key mapping

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,7 +3,7 @@ local map = vim.keymap.set
 map("i", "jk", "<ESC>", { desc = "Escape to normal mode", noremap = true, silent = true })
 map("t", "jk", [[<C-\><C-n>]], { desc = "Escape to normal mode in terminal", noremap = true, silent = true })
 
-map("n", "<leader>bz", "<cmd>tabedit %<cr>", { desc = "Zoom buffer into new tab", noremap = true, silent = true })
+map("n", "<leader>bz", "<cmd>tab sb<cr>", { desc = "Zoom buffer into new tab", noremap = true, silent = true })
 
 map("n", "<leader>tt", function()
 	vim.cmd("tabnew | terminal")
