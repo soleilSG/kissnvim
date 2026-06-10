74e280b14a0ddf566cd32ab127e2afa5c906f168
soleil <applosoleil@gmail.com>
2026-06-10 13:52:40 +0000

    Added configuration to lazy for supporting local plugins development

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,5 +1,5 @@
 vim.lsp.enable({
-	-- "lua_ls",
+	"lua_ls",
 	-- "clangd",
 	-- "jdtls",
 	-- "vtsls",
---
+++ lua/plugins/dev.lua
@@ -0,0 +1,11 @@
+return {
+	dir = "~/atelier/Nvim/kissnvim-plugin",
+	name = "kissnvim",
+	lazy = false,
+	dev = true,
+	--[[
+	config = function()
+		require("kissnvim").setup()
+	end,
+  --]]
+}
