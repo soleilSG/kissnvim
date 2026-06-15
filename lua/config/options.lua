26e9846a56caea3e0fa0983aa8e484c964222e89
soleil <applosoleil@gmail.com>
2026-06-15 04:49:52 +0000

    Moved feature of shortening tabpage title from kissnvim-plugin to config.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -28,3 +28,28 @@ opt.expandtab = true
 
 -- enable copy, paste to system clipboard by default
 opt.clipboard = "unnamedplus"
+
+-- Shorten tabpage lable on tabline
+function ConciseTabLine()
+	local s = ""
+	for i = 1, vim.fn.tabpagenr("$") do
+		-- Highlight current tab differently
+		if i == vim.fn.tabpagenr() then
+			s = s .. "%#TabLineSel#"
+		else
+			s = s .. "%#TabLine#"
+		end
+		-- Get buffer name for the active window in each tabpage
+		local buflist = vim.fn.tabpagebuflist(i)
+		local winnr = vim.fn.tabpagewinnr(i)
+		local bufname = vim.fn.bufname(buflist[winnr])
+		local filename = vim.fn.fnamemodify(bufname, ":t") -- ':t' isolates the file name
+
+		if filename == "" then
+			filename = "[No Name]"
+		end
+		s = s .. " " .. i .. ": " .. filename .. " "
+	end
+	return s
+end
+vim.o.tabline = "%!v:lua.ConciseTabLine()"
